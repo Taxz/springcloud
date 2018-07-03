89e4ab34c9ec931016a449630443744f8e898e48
Taxz <1240887908@qq.com>
2018-07-03 11:48:45 +0800

    qq邮箱配置属性

--- springamqp-mail-consumer/src/main/java/txz/study/example/config/EmailConfig.java
+++ springamqp-mail-consumer/src/main/java/txz/study/example/config/EmailConfig.java
@@ -34,8 +34,12 @@ public class EmailConfig {
         // 配置邮件服务器
         Properties props = new Properties();
         // 让服务器进行认证,认证用户名和密码是否正确
+        //认证机制开关，记得开启。
         props.put("mail.smtp.auth", "true");
+        //连接超时
         props.put("mail.smtp.timeout", "25000");
+        //TLS通讯协议开关，连接qq的SMTP服务器，需使用此通讯协议。
+        props.put("mail.smtp.starttls.enable", "true");
         mailSender.setJavaMailProperties(props);
         return mailSender;
     }
